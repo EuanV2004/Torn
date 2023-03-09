ab807b392239fbbd50a34166cded32b0258dc57b
IzzyDev <kishoreishaan@gmail.com>
2023-03-08 21:30:21 -0500

    Yeah the water puzzle now works
    No longer scuffed, I'm smart :)

--- Torn/Assets/Scripts/Puzzles/WaterBlocksMovement.cs
+++ Torn/Assets/Scripts/Puzzles/WaterBlocksMovement.cs
@@ -3,12 +3,27 @@ using UnityEngine;
 namespace Torn.Puzzles {
     public class WaterBlocksMovement : MonoBehaviour
     {
-        public enum MovementDirection {Left, Right, Down};
+        [SerializeField] private float playerSpeed;
 
-        [SerializeField] private MovementDirection movementDirection;
+        private void Update() {
+            MovePlayer();
+        }
 
-        public MovementDirection ReturnMovementDirection() {
-            return movementDirection;
+        private void MovePlayer() {
+            var horizontalInput = Input.GetAxisRaw("Horizontal");
+            var verticalInput = Input.GetAxisRaw("Vertical");
+
+            var horizontalMovement = horizontalInput * playerSpeed * Time.deltaTime;
+            var verticalMovement = verticalInput * playerSpeed * Time.deltaTime;
+
+            Vector3 newPos = transform.position + new Vector3(horizontalMovement, verticalMovement, 0f);
+            transform.position = newPos;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.CompareTag("WaterPuzzleGoal")) {
+                print("Won!");
+            }
         }
     }
 }
