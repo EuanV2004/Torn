a2686fc239752bbf0061fefe95f2ee7d299ca33b
EuanV2004 <euan.volkmann@ontariotechu.net>
2023-04-03 02:07:46 -0400

    Updated key indicator

--- Torn/Assets/Scripts/TEMPKeyCue.cs
+++ Torn/Assets/Scripts/TEMPKeyCue.cs
@@ -3,17 +3,30 @@ using UnityEngine;
 public class TEMPKeyCue : MonoBehaviour
 {
     [SerializeField] private GameObject keyCue;
+    [SerializeField] private bool hidden;
     private bool hasPassedOnce = false;
+
+    int keyCount;
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.E)) {
             keyCue.SetActive(false);
         }
+
+        keyCount = FindObjectOfType<Torn.Interact.PlayerInteract>().GetKeyCount();
+
+        if (keyCount >= 4)
+        {
+            hidden = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")){ //&& !hasPassedOnce) {
-            keyCue.SetActive(true);
+            if (!hidden)
+            {
+                keyCue.SetActive(true);
+            }
             //hasPassedOnce = true;
         }
     }
